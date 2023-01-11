455897318a1e1e81281d92ab8a8eae2eda054390
정은 <95029928+jungeuno@users.noreply.github.com>
2023-01-11 07:52:36 +0000

    create

--- Step 4/8958.py
+++ Step 4/8958.py
@@ -5,11 +5,11 @@ for i in range(n):
   answer = input()
   if answer[i] == 'X':
     r += 0
-  elif answer[i] == answer[i-1]:
+  elif answer[i] == answer[i+1]:
     r += i+1
   else:
     r += 1
-  result.append(r)
+  result[i].append(r)
 
 for i in result:
   print(i)
