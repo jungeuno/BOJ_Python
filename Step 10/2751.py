afedea687f5d4da71e65f9b885819597429d9c61
정은 <95029928+jungeuno@users.noreply.github.com>
2023-01-12 06:48:12 +0000

    update 2751.py

--- Step 10/2751.py
+++ Step 10/2751.py
@@ -1,12 +1,7 @@
+import sys
+input=sys.stdin.readline
 n = int(input())
-l = []
-for i in range(n):
-  num = int(input())
-  if num >= l[i]:
-    l[i+1].append(num)
-  else:
-    temp = l[i]
-    l[i].append(num)
-    l[i+1].append(temp)
+l = [int(input()) for i in range(n)]
+l.sort()
 for i in l:
   print(i)
