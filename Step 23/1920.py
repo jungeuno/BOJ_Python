4b0599bfd6e8282347d97929da3929f0ec856423
정은 <95029928+jungeuno@users.noreply.github.com>
2023-01-17 15:14:49 +0000

    update 1920.py

--- Step 23/1920.py
+++ Step 23/1920.py
@@ -8,6 +8,14 @@ start = 0
 mid = N//2
 end = N
 
-def binary_search():
+def binary_search(array1, array2, start, end):
   while start <= end:
+    for i in range(start, mid):
+      if array1[i] in array2:
+        print(1)
+      else:
+        break
+    mid = end
+    start = (mid)+1
+        
     
